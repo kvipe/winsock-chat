7446f16fd4756536d499f0abf00eed52173a9d44
kvipe <kvipe@mail.ru>
2018-07-28 16:26:53 +0300

    add ability to service several clients

--- client.c
+++ client.c
@@ -6,11 +6,22 @@
   
 #define PORT 8888
 #define IPADDR "127.0.0.1"
-  
+
+SOCKET connection;
+
+void client_thread()
+{
+	char buffer[256]; //Create buffer to hold messages up to 256 characters
+	while (true)
+	{
+		recv(connection, buffer, sizeof(buffer), NULL); //receive buffer
+		printf("%s\n", buff);
+	}
+}
+
 int main(int argc , char *argv[])
 {
 	WSADATA wsa;
-	SOCKET connection;
 	struct sockaddr_in client_addr;
 	int addrlen;
 	char *message;
@@ -25,6 +36,7 @@ int main(int argc , char *argv[])
     if((connection = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
     {
         printf("Could not create socket : %d" , WSAGetLastError());
+        return 1;
     }
 	
 	addrlen = sizeof(struct sockaddr_in);
@@ -35,13 +47,16 @@ int main(int argc , char *argv[])
 	if (connect(connection, (SOCKADDR*)&client_addr, addrlen) != 0) //If we are unable to connect...
 	{
 		printf("Failed to connect");
-		return 0; //Failed to Connect
+		return 1; //Failed to Connect
 	}
+	printf("Connection established");
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)client_thread, NULL, NULL, NULL); //Create the client thread that will receive any data that the server sends.
 	char buff[256];
 	while(true)
 	{
-		recv(connection, buff, sizeof(buff), NULL);
-		printf("%s\n", buff);
+		printf("Enter your message:");
+		fgets(buff, sizeof(buff), stdin);
+		send(connection, buffer, sizeof(buffer), NULL); //Send buffer
 	}
 
 	closesocket(connection);
--- server.c
+++ server.c
@@ -6,7 +6,27 @@
 
 #define PORT 8888
 #define IPADDR "127.0.0.1"
+#define MAXCLIENTS 5 
   
+
+SOCKET connections[MAXCLIENTS];
+int connections_counter = 0;
+
+void client_handler_thread(int index) //index = the index in the SOCKET Connections array
+{
+    char buffer[256]; //Buffer to receive and send out messages from/to the clients
+    while (true)
+    {
+        recv(connections[index], buffer, sizeof(buffer), NULL); //get message from client
+        for (int i = 0; i < connections_counter; i++) //For each client connection
+        {
+            if (i == index) //Don't send the chat message to the same user who sent it
+                continue;
+            send(connections[i], buffer, sizeof(buffer), NULL);//send the chat message to this client
+        }
+    }
+}
+
 int main(int argc , char *argv[])
 {
     WSADATA wsa;
@@ -33,6 +53,7 @@ int main(int argc , char *argv[])
     if((host_sock = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
     {
         printf("Could not create socket : %d" , WSAGetLastError());
+        return 1;
     }
   
     printf("Socket created.\n");
@@ -47,12 +68,13 @@ int main(int argc , char *argv[])
     if( bind(host_sock ,(struct sockaddr *)&server_addr , sizeof(server_addr)) == SOCKET_ERROR)
     {
         printf("Bind failed with error code : %d" , WSAGetLastError());
+        return 1;
     }
       
-    puts("Bind done");
+    puts("Socket binded.");
   
     //Listen to incoming connections
-    listen(host_sock , 3);
+    listen(host_sock , MAXCLIENTS);
       
     //Accept and incoming connection
     puts("Waiting for incoming connections...");
@@ -60,26 +82,25 @@ int main(int argc , char *argv[])
 	//besides that accept returns client parameters (sockaddr_in structure as well as server structure)
 	//client's address have to for non anonymous
     // отдельный канал для взаимодействия
-    new_conn_sock = accept(host_sock, (struct sockaddr *)&client_addr, &addrlen);
-    if (new_conn_sock == INVALID_SOCKET)
+    for(int i = 0; i < MAXCLIENTS; i++)
     {
-        printf("accept failed with error code : %d" , WSAGetLastError());
-    }
-	else
-	{		
-		puts("Connection accepted");
-	}
-    //Reply to client
-    char buff[256] = "Welcome to kvipe server!";
-    while(true)
-    {
-        printf("Message to client:");
-        fgets(buff,sizeof(buff), stdin);
-        send(new_conn_sock , buff , sizeof(buff) , NULL);
-    }
+        new_conn_sock = accept(host_sock, (struct sockaddr *)&client_addr, &addrlen);
+        if (new_conn_sock == INVALID_SOCKET)
+        {
+            printf("accept failed with error code : %d" , WSAGetLastError());
+        }
+        else
+        {       
+            puts("Client connected");
+            char buff[256] = "Welcome to kvipe server!";
+            send(new_conn_sock , buff , sizeof(buff) , NULL);
+            connections[i] = new_conn_sock;
+            connections_counter += 1;
+            CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)client_handler_thread, (LPVOID)(i), NULL, NULL);
+        }
 
-    getchar();
-  
+        closesocket(new_conn_sock);
+    }
     closesocket(host_sock);
     WSACleanup();
       
